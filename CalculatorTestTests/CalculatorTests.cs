b886cbb60fc72e2bf945eeb4b3cc897779db84f8
Poul Henriksen <pohe@easj.dk>
2019-03-11 09:35:08 +0100

    Add project files.

---
+++ CalculatorTestTests/CalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTestPositive()
+        {
+            //Arrange
+            Calculator c = new Calculator();
+
+            //Act
+            int t1 = 12;
+            int t2 = 8;
+            int expectedResult = 20;
+            int actualResult = c.Add(t1, t2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void AddTestNegative()
+        {
+            //Arrange
+            Calculator c = new Calculator();
+
+            //Act
+            int t1 = -12;
+            int t2 = -8;
+            int expectedResult = -20;
+            int actualResult = c.Add(t1, t2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            // Arrange
+            Calculator c = new Calculator();
+
+            //Act
+            int t1 = 12;
+            int t2 = 8;
+            int expectedResult = 4;
+            int actualResult = c.Minus(t1, t2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Sandbox/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Calculator
+    {
+        public int Add(int tal1, int tal2)
+        {
+            int result = tal1 + tal2;
+            return result;
+        }
+
+        public int Minus(int tal1, int tal2)
+        {
+            int result = tal1 - tal2;
+            return result;
+        }
+
+    }
+}
